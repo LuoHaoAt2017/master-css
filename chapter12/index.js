34b1f21f5b7422abe21788cb3257b34c862c0ba3
LuoHao-RenRen <hluo5206@gmail.com>
2020-03-06 20:44:19 +0800

    fix: delete node_modules

--- chapter12/index.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-  const root = document.querySelector('#root');
-  root.addEventListener('mouseenter', function () {
-    root.className += 'large';
-  });
-  root.addEventListener('mouseout', function () {
-    root.className = '';
-  });
-};
