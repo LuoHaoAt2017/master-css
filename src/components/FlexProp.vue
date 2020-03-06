4e0466df923eacbc9ef334edbef515789b5afdc2
LuoHao-RenRen <hluo5206@gmail.com>
2020-03-06 22:02:51 +0800

    fix: 引入scss

---
+++ src/components/FlexProp.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="container">
+        <div class="item1 item"></div>
+        <div class="item2 item"></div>
+        <div class="item3 item"></div>
+        <div class="item4 item"></div>
+        <div class="item5 item"></div>
+    </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+      }
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+
+    .container {
+        display: flex;
+        margin: auto;
+        background: #EEEEEE;
+    }
+
+    .item {
+        border: 1px solid #aaa;
+        height: 150px;
+        margin: 0 5px;
+    }
+
+    .item1 {
+        flex-basis: 300px;
+        flex-grow: 2;
+        flex-shrink: 2;
+    }
+
+    .item2, .item3 {
+        flex-basis: 150px;
+        flex-grow: 1;
+        flex-shrink: 1;
+    }
+
+    .item4, .item5 {
+        flex-basis: 50px;
+        flex-grow: 3;
+        flex-shrink: 0;
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,21 @@
 import VueRouter from 'vue-router';
 import BoxShadow from '@/components/BoxShadow.vue';
+import FlexProp from '@/components/FlexProp.vue';
+import TextCss from '@/components/TextCss.vue';
 
 const routes = [
   {
     path: '/',
-    redirect: '/box-shadow'
+    redirect: '/flex-css'
   }, {
     path: '/box-shadow',
     component: BoxShadow
+  }, {
+    path: '/flex-css',
+    component: FlexProp
+  }, {
+    path: '/text-css',
+    component: TextCss
   }
 ];
 
